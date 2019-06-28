bc90185e6456e5b1af74e8aead5b25e80fcd9195
Jan Ulferts <julferts@mesosphere.com>
2019-06-28 12:42:34 +0200

    initial windows example

--- aws/windows-agent/main.tf
+++ aws/windows-agent/main.tf
@@ -6,7 +6,7 @@ data "http" "whatismyip" {
 }
 
 locals {
-  cluster_name = "windows"
+  cluster_name = "generic-dcos-ee-demo"
 }
 
 module "dcos" {
@@ -22,18 +22,19 @@ module "dcos" {
   admin_ips           = ["${data.http.whatismyip.body}/32"]
 
   num_masters        = "1"
-  num_private_agents = "0"
+  num_private_agents = "1"
   num_public_agents  = "1"
 
+  dcos_instance_os        = "centos_7.5"
+  bootstrap_instance_type = "m4.xlarge"
+
   dcos_variant              = "ee"
-  dcos_version              = "1.13.0"
+  dcos_version              = "1.13.1"
   dcos_license_key_contents = "${file("~/license.txt")}"
 
-  ansible_bundled_container = "mesosphere/dcos-ansible-bundle:latest"
-
-  # this should be additional_windows_agent_ips. And we need a way to specify
-  # their passwords
-  # additional_private_agent_ips = ["${module.windowsagent.private_ips}"]
+  # provide a SHA512 hashed password, here "deleteme"
+  dcos_superuser_password_hash = "$6$rounds=656000$YSvuFmasQDXheddh$TpYlCxNHF6PbsGkjlK99Pwxg7D0mgWJ.y0hE2JKoa61wHx.1wtxTAHVRHfsJU9zzHWDoE08wpdtToHimNR9FJ/"
+  dcos_superuser_username      = "demo-super"
 }
 
 module "windowsagent" {
@@ -43,7 +44,7 @@ module "windowsagent" {
   cluster_name           = "${local.cluster_name}"
   hostname_format        = "%[3]s-winagent%[1]d-%[2]s"
   aws_subnet_ids         = ["${module.dcos.infrastructure.vpc.subnet_ids}"]
-  aws_security_group_ids = ["${module.dcos.infrastructure.security_groups.internal}"]
+  aws_security_group_ids = ["${module.dcos.infrastructure.security_groups.internal}", "${module.dcos.infrastructure.security_groups.admin}"]
   aws_key_name           = "${module.dcos.infrastructure.aws_key_name}"
 
   # aws_extra_volumes = [
@@ -69,68 +70,48 @@ resource "local_file" "ansible_inventory" {
 
   content = <<EOF
 [bootstraps]
-${join("\n", module.dcos.infrastructure.bootstrap.public_ip)}
-
+${module.dcos.infrastructure.bootstrap.public_ip}
 [masters]
 ${join("\n", module.dcos.infrastructure.masters.public_ips)}
-
 [agents_private]
 ${join("\n", module.dcos.infrastructure.private_agents.public_ips)}
-
-[agents_windows]
-ansible_connection=winrm
-ansible_winrm_transport
-${formatlist("%s ansible_password=%s", module.windowsagent.public_ips, module.windowsagent.public_ips)}
-
 [agents_public]
 ${join("\n", module.dcos.infrastructure.public_agents.public_ips)}
-
+[agents_windows]
+${join("\n",formatlist("%s ansible_user=${module.windowsagent.os_user} ansible_password=%s", module.windowsagent.public_ips, module.windowsagent.windows-passwords))}
 [bootstraps:vars]
 node_type=bootstrap
-
 [masters:vars]
 node_type=master
 dcos_legacy_node_type_name=master
-
 [agents_private:vars]
 node_type=agent
 dcos_legacy_node_type_name=slave
-
 [agents_public:vars]
 node_type=agent_public
 dcos_legacy_node_type_name=slave_public
-
+[agents_windows:vars]
+ansible_connection=winrm
+ansible_winrm_transport=basic
+ansible_winrm_server_cert_validation=ignore
 [agents:children]
 agents_private
 agents_public
 agents_windows
-
 [dcos:children]
 bootstraps
 masters
 agents
 agents_public
-agents_windows
-
-EOF
-}
-
-resource "local_file" "vars_file" {
-  filename = "./dcos.yml"
-
-  content = <<EOF
----
-${local.ansible_additional_config}
-
-# Core Module vars. dcos['config'] needed for some of our Roles.
-dcos:
-  config:
-  ${indent(4, module.dcos.config)}
-
 EOF
 }
 
 output "masters_dns_name" {
   description = "This is the load balancer address to access the DC/OS UI"
   value       = "${module.dcos.masters-loadbalancer}"
+}
+
+output "passwords" {
+  description = "This is the load balancer address to access the DC/OS UI"
+  value       = "${module.windowsagent.windows-passwords}"
 }
