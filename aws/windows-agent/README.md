b29c41eec05916c0b68f7f02f60824fa54d09e9f
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-07-02 19:54:48 +0200

    changed to new alpha status content

--- aws/windows-agent/README.md
+++ aws/windows-agent/README.md
@@ -3,7 +3,7 @@
 This is an early example how to use and integrate windows agents. If you're not
 an active developer of this project we do not suggest to use it.
 
-## Ansible run not yet fully integrated.
+## Terraform usage
 
 You need our Terraformfile based terraform 0.11 fork: https://github.com/fatz/terraform/releases/tag/v0.11.14-mesosphere
 
@@ -12,23 +12,9 @@ terraform init -upgrade
 terraform apply
 ```
 
-wait for the cluster to be spawned. It will also spin up a windows instance but it is not yet installed.
+wait for the cluster to be spawned.
+It will also spin up a windows instance and uses the `ansible_bundled_container` docker image.
 
-To install it we use the by terraform generated inventory file `./inventory` and the dcos-ansible windows branch.
-
-```
-git clone git@github.com:dcos/dcos-ansible.git
-cd dcos-ansible
-git fetch && git checkout feature/windows-support
-cp group_vars/all/dcos.yaml.example group_vars/all/dcos.yaml
-```
-
-make sure you've ansible and winrm support installed
-```
-pip install ansible==2.7.8 jmespath pywinrm==0.3.0
-```
-
-now you can run ansible with the provided inventory file
-```
-ansible-playbook -vvv -i ../inventory -l agents_windows dcos.yml -e /group_vars/all/dcos.yaml
-```
+The `ansible_bundled_container` can be replaced with your own provided docker image, to test the ansible run
+which is fully integrated into the terraform apply process.
+It will be pulled onto the bootstrap node and being executed there.
--- aws/windows-agent/main.tf
+++ aws/windows-agent/main.tf
@@ -1,4 +1,6 @@
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
 # Used to determine your public IP for forwarding rules
 data "http" "whatismyip" {
@@ -25,93 +27,35 @@ module "dcos" {
   num_private_agents = "1"
   num_public_agents  = "1"
 
-  dcos_instance_os        = "centos_7.5"
-  bootstrap_instance_type = "m4.xlarge"
-
+  dcos_instance_os          = "centos_7.5"
   dcos_variant              = "ee"
-  dcos_version              = "1.13.1"
+  dcos_version              = "1.13.0"
   dcos_license_key_contents = "${file("~/license.txt")}"
+  ansible_bundled_container = "sebbrandt87/dcos-ansible-bundle:windows-support"
 
-  # provide a SHA512 hashed password, here "deleteme"
-  dcos_superuser_password_hash = "$6$rounds=656000$YSvuFmasQDXheddh$TpYlCxNHF6PbsGkjlK99Pwxg7D0mgWJ.y0hE2JKoa61wHx.1wtxTAHVRHfsJU9zzHWDoE08wpdtToHimNR9FJ/"
-  dcos_superuser_username      = "demo-super"
+  additional_windows_private_agent_ips       = ["${concat(module.winagent.private_ips)}"]
+  additional_windows_private_agent_passwords = ["${concat(module.winagent.windows_passwords)}"]
 }
 
-module "windowsagent" {
+module "winagent" {
   source  = "dcos-terraform/windows-instance/aws"
-  version = "~> 0.2.0"
+  version = "~> 0.2.1"
+
+  providers = {
+    aws = "aws"
+  }
 
   cluster_name           = "${local.cluster_name}"
   hostname_format        = "%[3]s-winagent%[1]d-%[2]s"
   aws_subnet_ids         = ["${module.dcos.infrastructure.vpc.subnet_ids}"]
-  aws_security_group_ids = ["${module.dcos.infrastructure.security_groups.internal}", "${module.dcos.infrastructure.security_groups.admin}"]
+  aws_security_group_ids = ["${module.dcos.infrastructure.security_groups.internal}"]
   aws_key_name           = "${module.dcos.infrastructure.aws_key_name}"
-
-  # aws_extra_volumes = [
-  #   {
-  #     size        = "100"
-  #     type        = "gp2"
-  #     iops        = "3000"
-  #     device_name = "/dev/xvdi"
-  #   },
-  #   {
-  #     size        = "1000"
-  #     type        = ""          # Use AWS default.
-  #     iops        = "0"         # Use AWS default.
-  #     device_name = "/dev/xvdj"
-  #   },
-  # ]
+  aws_instance_type      = "m5.xlarge"
 
   num = "1"
-}
-
-resource "local_file" "ansible_inventory" {
-  filename = "./inventory"
-
-  content = <<EOF
-[bootstraps]
-${module.dcos.infrastructure.bootstrap.public_ip}
-[masters]
-${join("\n", module.dcos.infrastructure.masters.public_ips)}
-[agents_private]
-${join("\n", module.dcos.infrastructure.private_agents.public_ips)}
-[agents_public]
-${join("\n", module.dcos.infrastructure.public_agents.public_ips)}
-[agents_windows]
-${join("\n",formatlist("%s ansible_user=${module.windowsagent.os_user} ansible_password=%s", module.windowsagent.public_ips, module.windowsagent.windows-passwords))}
-[bootstraps:vars]
-node_type=bootstrap
-[masters:vars]
-node_type=master
-dcos_legacy_node_type_name=master
-[agents_private:vars]
-node_type=agent
-dcos_legacy_node_type_name=slave
-[agents_public:vars]
-node_type=agent_public
-dcos_legacy_node_type_name=slave_public
-[agents_windows:vars]
-ansible_connection=winrm
-ansible_winrm_transport=basic
-ansible_winrm_server_cert_validation=ignore
-[agents:children]
-agents_private
-agents_public
-agents_windows
-[dcos:children]
-bootstraps
-masters
-agents
-agents_public
-EOF
 }
 
 output "masters_dns_name" {
   description = "This is the load balancer address to access the DC/OS UI"
   value       = "${module.dcos.masters-loadbalancer}"
-}
-
-output "passwords" {
-  description = "This is the load balancer address to access the DC/OS UI"
-  value       = "${module.windowsagent.windows-passwords}"
 }
