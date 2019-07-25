f4761661cfbc8393393d34fdf54ec258a3430a5e
Sergii Matus <sergii_matus@epam.com>
2019-07-25 13:26:39 +0300

    SM : modified docker image, and removed Terraformfile upon 'windows-instance' module officialy released

--- aws/windows-agent/README.md
+++ aws/windows-agent/README.md
@@ -1,21 +1,16 @@
-# ALPHA STATE - DO NOT USE THIS EXAMPLE
+# Windows Agent support (Alpha state example)
 
 This is an early example how to use and integrate windows agents. If you're not
 an active developer of this project we do not suggest to use it.
 
-## Terraform usage
-_____________
-**You need our Terraformfile based terraform 0.11 fork: https://github.com/fatz/terraform/releases/tag/v0.11.14-mesosphere**
-_____________
+## Usage
+In order to get Windows instance span up, use latest `main.tf` and specify number of Windows Agents at `num` field.
+Currently `main.tf` contains `ansible_bundled_container` parameter which refers 
+to docker image with windows-related changes (tag `feature-windows-support-${commit}` indicates about it).
 
-```
-terraform init -upgrade
-terraform apply
-```
-
-wait for the cluster to be spawned.
-It will also spin up a windows instance and uses the `ansible_bundled_container` docker image.
+The proper image can be found at [mesosphere/dcos-ansible-bundle](https://hub.docker.com/r/mesosphere/dcos-ansible-bundle/tags)
+For instance, `ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"` 
 
 The `ansible_bundled_container` can be replaced with your own provided docker image, to test the ansible run
-which is fully integrated into the terraform apply process.
+which is fully integrated into the terraform apply process. 
 It will be pulled onto the bootstrap node and being executed there.
--- aws/windows-agent/main.tf
+++ aws/windows-agent/main.tf
@@ -8,12 +8,12 @@ data "http" "whatismyip" {
 }
 
 locals {
-  cluster_name = "generic-dcos-ee-demo"
+  cluster_name = "generic-dcos-demo"
 }
 
 module "dcos" {
   source  = "dcos-terraform/dcos/aws"
-  version = "~> 0.2.0"
+  version = ">= 0.2.0"
 
   cluster_name        = "${local.cluster_name}"
   ssh_public_key_file = "~/.ssh/id_rsa.pub"
@@ -31,19 +31,20 @@ module "dcos" {
   dcos_variant = "open"
 
   dcos_version              = "1.13.0"
-  ansible_bundled_container = "sebbrandt87/dcos-ansible-bundle:windows-support"
+  //TO DO : change a tag once a new build of docker image available
+  ansible_bundled_container = "mesosphere/dcos-ansible-bundle:feature-windows-support-039d79d"
 
   # provide a SHA512 hashed password, here "deleteme"
   dcos_superuser_password_hash = "$6$rounds=656000$YSvuFmasQDXheddh$TpYlCxNHF6PbsGkjlK99Pwxg7D0mgWJ.y0hE2JKoa61wHx.1wtxTAHVRHfsJU9zzHWDoE08wpdtToHimNR9FJ/"
   dcos_superuser_username      = "demo-super"
 
-  additional_windows_private_agent_ips       = ["${concat(module.winagent.private_ips)}"]
-  additional_windows_private_agent_passwords = ["${concat(module.winagent.windows_passwords)}"]
+  additional_windows_private_agent_ips       = ["${concat(module.windows-instance.private_ips)}"]
+  additional_windows_private_agent_passwords = ["${concat(module.windows-instance.windows_passwords)}"]
 }
 
-module "winagent" {
+module "windows-instance" {
   source  = "dcos-terraform/windows-instance/aws"
-  version = "~> 0.2.1"
+  version = ">= 0.0.1"
 
   providers = {
     aws = "aws"
@@ -66,5 +67,5 @@ output "masters_dns_name" {
 }
 
 output "windows_passwords" {
-  value = "${module.winagent.windows_passwords}"
+  value = "${module.windows-instance.windows_passwords}"
 }
