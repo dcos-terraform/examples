458d06ae349cc346f4385e3ee7f80d93b4b2c556
Jan Ulferts <julferts@mesosphere.com>
2020-06-16 14:57:59 +0200

    fix: use 2.1.0 release

--- aws/simple/main.tf
+++ aws/simple/main.tf
@@ -30,7 +30,7 @@ module "dcos" {
   bootstrap_instance_type = "m4.xlarge"
 
   dcos_variant              = "ee"
-  dcos_version              = "2.0.4"
+  dcos_version              = "2.1.0"
   dcos_license_key_contents = file("~/license.txt")
 
   # provide a SHA512 hashed password, here "deleteme"
